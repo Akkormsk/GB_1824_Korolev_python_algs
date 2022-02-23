9b3de78289dfb5d0396c50cc5235cdff0492b6e4
d_kor <akkormsk@gmail.com>
2022-02-23 17:43:34 +0300

    Задания 1-6

--- Lesson_2.py
+++ Lesson_2.py
@@ -1,17 +1,13 @@
-import random
-
-
 def num_1(input_n: int) -> str:
     even = 0
-    odd = 0
+    n = 0
     while input_n > 0:
         num = input_n % 10
         if num % 2 == 0:
             even += 1
-        else:
-            odd += 1
+        n += 1
         input_n //= 10
-    res = f'Четных - {even}, нечетных - {odd}'
+    res = f'Четных - {even}, нечетных - {n - even}'
     return res
 
 
@@ -24,7 +20,7 @@ def num_2(input_n: int) -> int:
     return res
 
 
-def num_3(input_n: int) -> int:
+def num_3(input_n: int) -> float:
     res = 0
     f = 1
     for _ in range(input_n):
@@ -33,10 +29,22 @@ def num_3(input_n: int) -> int:
     return res
 
 
+def num_3_rec(n: int) -> float:
+    if n == 0:
+        return 0
+    else:
+        return num_3_rec(n - 1) + (-0.5) ** (n - 1)
+
+
 def num_4(input_n: int) -> bool:
-    res_1 = sum([n + 1 for n in range(input_n)])
+    def helper(n: int) -> int:
+        if n == 0:
+            return 0
+        else:
+            return helper(n - 1) + n
+
     res_2 = input_n * (input_n + 1) / 2
-    check = True if res_1 == res_2 else False
+    check = True if helper(input_n) == res_2 else False
     return check
 
 
@@ -53,18 +61,30 @@ def num_5() -> int:
 
 
 def num_6(nums: str) -> str:
+    def helper(n: int) -> int:
+        if n == 0:
+            return 0
+        else:
+            m = n % 10
+            return helper(n//10) + m
     num_list = list(map(int, nums.split(',')))
-    res_list = [0]*(len(num_list))
-    max_pair = [0, 0]
-    for i, num in enumerate(num_list):
-        while num > 0:
-            n = num % 10
-            num //= 10
-            res_list[i] += n
-        if res_list[i] > max_pair[0]:
-            max_pair[0] = res_list[i]
-            max_pair[1] = i
-    return f'Наибольшая сумма {max_pair[0]} у числа {num_list[max_pair[1]]}'
+    smax = helper(num_list[0])
+    for i in range(len(num_list)):
+        if helper(num_list[i]) > smax:
+            smax = helper(num_list[i])
+            imax = i
+    return f'Наибольшая сумма {smax} у числа {num_list[imax]}'
+
+
+class Solution:
+    def reverseString(self, s: list[str]):
+        if not s:
+            return []
+        else:
+            n = s[0]
+            s = self.reverseString(s[1:])
+            s.append(n)
+            return s
 
 
 if __name__ == '__main__':
@@ -73,8 +93,17 @@ if __name__ == '__main__':
         # print(num_1(int(input('Введите число: '))))
         # print(num_2(int(input('Введите число: '))))
         # print(num_3(int(input('Введите количество элементов: '))))
+        # print(num_3_rec(int(input('Введите количество элементов: '))))
         # print(num_4(int(input('Введите число: '))))
         # print(num_5())
-        # print(num_6(input('Введите числа через запятую: ')))
+        print(num_6(input('Введите числа через запятую: ')))
+        #
+        # Задание на рекурсию - так и не получилось решить на Литкод, хотя в pycharm работает.
+        # Там выдаёт output такой же как input
+        s = ["h", "e", "l", "l", "o"]
+        s_2 = ["H", "a", "n", "n", "a", "h"]
+        sol = Solution()
+        print(sol.reverseString(s))
+        print(sol.reverseString(s_2))
     except Exception as e:
         print(e)
